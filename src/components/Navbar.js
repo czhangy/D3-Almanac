e6763efc6d456faa42bf8e781fd778aaf8813553
Charles Zhang <charleszhangmb@gmail.com>
2021-05-04 09:46:56 -0700

    Start

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "../styles/navbar.css";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        <div className="app-logo">D3 Almanac</div>
+        
+      </div>
+    );
+  }
+}
